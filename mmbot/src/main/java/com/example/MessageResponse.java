d3907aae70168386894f49eee75bd0de43afda2b
hanjaegyeong <hjg000223@gmail.com>
2024-07-11 15:03:18 +0900

    feat: MessageResponse 추가

---
+++ mmbot/src/main/java/com/example/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MessageResponse {
+    private final String result;
+
+    public MessageResponse(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
